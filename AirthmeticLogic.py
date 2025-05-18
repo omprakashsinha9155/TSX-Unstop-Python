48b848a90cdd906e72051371a80b26014de56bd5
Om Prakash Sinha <142241301+omprakashsinha9155@users.noreply.github.com>
2025-05-18 23:11:02 +0530

    Create AirthmeticLogic.py

---
+++ AirthmeticLogic.py
@@ -0,0 +1,45 @@
+#Arithmetic operators
+
+# Addition
+print(5 + 3)     
+
+# Subtraction
+print(10 - 4)    
+
+# Multiplication
+print(6 * 7)     
+
+# Division (float)
+print(15 / 2)   
+
+# Floor Division
+print(15 // 2)
+
+# Modulus (Remainder)
+print(15 % 2)   
+
+# Exponentiation (Power)
+print(2 ** 3)    
+
+#comparison operators
+
+print(5 == 5)   
+print(10 != 3)  
+print(7 > 4)     
+print(4 < 2)     
+print(6 >= 6)   
+print(3 <= 1)    
+
+# Logical operator
+
+x = 10
+y = 5
+
+# AND
+print(x > 5 and y < 10)  
+
+# OR
+print(x < 5 or y < 10)   
+
+# NOT
+print(not(x < 5))
