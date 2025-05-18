b7473f09bff78a9a99e6cd67e2eccf368ecf9d48
Om Prakash Sinha <142241301+omprakashsinha9155@users.noreply.github.com>
2025-05-18 23:12:18 +0530

    Create GreaterNumber.py

---
+++ GreaterNumber.py
@@ -0,0 +1,11 @@
+num1= int (input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+
+if num1>num2:
+    print(f"{num1} is greater than {num2} ")
+
+else:
+    print(f"{num2} is greater then {num1}")
+
+if num1==num2:
+    print("Both the numbers are equal!  ")
